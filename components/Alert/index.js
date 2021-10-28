e5cc178f397d132532b9c9e628ca6325da4630a5
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-28 11:47:31 -0300

    Add Alert component

--- components/Alert/index.js
+++ components/Alert/index.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react'
 import styles from './index.module.css'
+import { CSSTransition } from 'react-transition-group'
 
-const Alert = ({ Icon, message = 'Example text for the alert' }) => {
+const Alert = ({ active, Icon, message = 'Example text for the alert' }) => {
+  const [activeAlert, setActiveAlert] = useState(active)
+
   return (
-    <div className={styles.alertWrapper}>
-      <div className={styles.alertContainer}>
-        {Icon && <Icon className={styles.icon} />}
-        <p className={styles.message}>{message}</p>
-      </div>
-    </div>
+    <CSSTransition
+      in={activeAlert}
+      classNames={{
+        enter: styles.alertEnter,
+        enterActive: styles.alertEnterActive,
+        exit: styles.alertExit,
+        exitActive: styles.alertExitActive,
+      }}
+      timeout={0}
+      unmountOnExit
+    >
+      <button onClick={() => setActiveAlert(false)} className={styles.alertWrapper}>
+        <div className={styles.alertContainer}>
+          {Icon && <Icon className={styles.icon} />}
+          <p className={styles.message}>{message}</p>
+        </div>
+      </button>
+    </CSSTransition>
   )
 }
 
