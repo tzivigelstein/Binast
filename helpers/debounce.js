394da97f18a417ed6f9b81aead964d6b22252b20
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-01-26 10:57:05 -0300

    Create debounce utility

---
+++ helpers/debounce.js
@@ -0,0 +1,14 @@
+export default function debounce(callback, time) {
+  let timeout = null
+  return function executedCallback(...args) {
+    const debounced = () => {
+      timeout = null
+
+      callback(...args)
+    }
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(debounced, time)
+  }
+}
