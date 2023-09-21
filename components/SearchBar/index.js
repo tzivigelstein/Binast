702b46e87818522f3e8c2f5a247cc5843177a08f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-09-21 18:21:32 -0300

    fixes

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,38 +1,37 @@
-import { useState, useMemo } from 'react'
-import styles from './index.module.css'
-import Image from 'next/image'
-import { ChevronDown, ChevronUp, Search } from '../Icons'
-import debounce from '../../helpers/debounce'
-import Spinner from '../Spinner'
-import useCrypto from '../../hooks/useCrypto'
-import { parseData } from '../../helpers/parseCryptos'
-import InputSelect from '../InputSelect'
+import {useState, useMemo} from "react"
+import styles from "./index.module.css"
+import Image from "next/image"
+import {Search} from "../Icons"
+import debounce from "../../helpers/debounce"
+import Spinner from "../Spinner"
+import useCrypto from "../../hooks/useCrypto"
+import {parseData} from "../../helpers/parseCryptos"
 
 const SEARCH_AFTER_500_MS = 500
 
 const SearchBar = () => {
-  const { setSelectedCrypto } = useCrypto()
+  const {setSelectedCrypto} = useCrypto()
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [focus, setFocus] = useState(false)
 
-  const [selectedCurrency, setSelectedCurrency] = useState('USD')
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("")
 
-  async function handleSearch({ value }) {
+  async function handleSearch({value}) {
     setLoading(true)
 
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${value}&tsyms=${selectedCurrency}`
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${value}&tsyms=USD`
 
     await fetch(url)
       .then(response => response.json())
       .then(response => {
-        const { RAW: rawData, Response: error } = response
+        const {RAW: rawData, Response: error} = response
 
-        if (error === 'Error') throw new Error("Couldn't find crypto 🤔")
+        if (error === "Error") throw new Error("Couldn't find crypto 🤔")
 
         const data = Object.values(rawData)[0]
-        setData(() => parseData({ RAW: data }))
+        setData(() => parseData({RAW: data}))
       })
       .finally(() => setLoading(false))
       .catch(error => {
@@ -41,16 +40,17 @@ const SearchBar = () => {
       })
   }
 
-  const debounced = useMemo(() => debounce(handleSearch, SEARCH_AFTER_500_MS), [selectedCurrency])
+  const debounced = useMemo(() => debounce(handleSearch, SEARCH_AFTER_500_MS), [])
 
-  const handleInputChange = ({ target: { value } }) => {
+  const handleInputChange = ({target: {value}}) => {
     setError(null)
+    setFocus(true)
 
     const parsedValue = value.trim().toUpperCase()
 
     setSearch(parsedValue)
-    if (parsedValue === '') return
-    debounced({ value: parsedValue })
+    if (parsedValue === "") return
+    debounced({value: parsedValue})
   }
 
   function handleResultClick() {
@@ -68,24 +68,27 @@ const SearchBar = () => {
             className={styles.input}
             placeholder="Search Ticker"
             type="text"
+            onBlur={() => setFocus(false)}
+            onFocus={() => setFocus(true)}
           />
-          {/* <InputSelect setSelectedCurrency={setSelectedCurrency} /> */}
         </div>
 
-        <div className={styles.resultsList}>
-          <p className={styles.resultsForHelper}>Results for {`"${search}"`}</p>
-          {loading && <Spinner />}
-          {error && <p>{error?.message || 'Error'}</p>}
-          {data && !loading && !error && (
-            <button onClick={handleResultClick} className={styles.result}>
-              <div className={styles.coinDataContainer}>
-                <Image width={32} height={32} src={`https://cryptocompare.com/${data.imageUrl}`} />
-                <span>{data.id}</span>
-              </div>
-              <p>{data.price}</p>
-            </button>
-          )}
-        </div>
+        {search !== "" && focus && (
+          <div className={styles.resultsList}>
+            <p className={styles.resultsForHelper}>Results for "{search}"</p>
+            {loading && <Spinner />}
+            {error && <p>{error?.message || "Error"}</p>}
+            {data && !loading && !error && (
+              <button onClick={handleResultClick} className={styles.result}>
+                <div className={styles.coinDataContainer}>
+                  <Image width={32} height={32} src={`https://cryptocompare.com/${data.imageUrl}`} />
+                  <span>{data.id}</span>
+                </div>
+                <p>{data.price}</p>
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </>
   )
