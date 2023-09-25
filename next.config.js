a1a4043ff95d2da4f1aaafde43074d69385fe74b
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-09-25 17:55:21 -0300

    add aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const path = require("path")
+
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cryptocompare.com', 'vhv.rs'],
+    domains: ["cryptocompare.com", "vhv.rs"],
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.join(__dirname, "components"),
+      "@utils": path.join(__dirname, "utils"),
+      "@helpers": path.join(__dirname, "helpers"),
+      "@hooks": path.join(__dirname, "hooks"),
+    }
+
+    return config
   },
 }
